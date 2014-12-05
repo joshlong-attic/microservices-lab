6153b589012e9bf484c486754823262bd33a5df3
Matt Stine <mstine@gopivotal.com>
2014-12-05 11:44:01 -0500

    add manifest, restore main method

--- bootcamp/src/main/java/bootiful/Application.java
+++ bootcamp/src/main/java/bootiful/Application.java
@@ -39,9 +39,9 @@ public class Application {
         };
     }
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
 
 @Controller
