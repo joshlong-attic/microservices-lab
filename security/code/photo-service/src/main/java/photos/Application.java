ba4218b0d2cc3a575a4fa1db090dd62270d911ee
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-12-04 23:38:34 -0500

    addding initial security support for photos ervice.

--- security/code/photo-service/src/main/java/photos/Application.java
+++ security/code/photo-service/src/main/java/photos/Application.java
@@ -2,11 +2,9 @@ package photos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.http.HttpHeaders;
@@ -23,9 +21,11 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableOAuth2Resource
 public class Application {
 
     public static void main(String[] args) {
@@ -34,31 +34,34 @@ public class Application {
 }
 
 @RestController
-@RequestMapping("/{userId}/photo")
+@RequestMapping("/photo")
 class PhotoRestController {
 
     @Autowired
     private GridFsTemplate gridFsTemplate;
 
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
-    ResponseEntity<?> set(String userId,
+    ResponseEntity<?> set(Principal principal,
                           @RequestParam MultipartFile multipartFile,
                           UriComponentsBuilder uriBuilder) throws IOException {
+
+        String userId  = principal.getName() ;
 
         try (InputStream inputStream = multipartFile.getInputStream()) {
             this.gridFsTemplate.store(inputStream, userId);
         }
-        URI uri = uriBuilder.path("/{userId}/photo").buildAndExpand(userId).toUri();
+        URI uri = uriBuilder.path("/photo").buildAndExpand(userId).toUri();
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(uri);
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    ResponseEntity<Resource> get(String userId) {
+    ResponseEntity<Resource> get(Principal principal ) {
+        String userId = principal.getName();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.IMAGE_JPEG);
-        return new ResponseEntity<Resource>(
+        return new ResponseEntity<>(
                 this.gridFsTemplate.getResource(userId), httpHeaders, HttpStatus.OK);
     }
 }
