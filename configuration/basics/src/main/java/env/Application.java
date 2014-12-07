7f88b200397a05ba6928515f2ac4404c7ce3d2e2
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-12-06 19:20:19 -0800

    p

--- configuration/basics/src/main/java/env/Application.java
+++ configuration/basics/src/main/java/env/Application.java
@@ -1,6 +1,7 @@
 package env;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
