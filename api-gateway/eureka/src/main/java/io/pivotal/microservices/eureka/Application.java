a3c79477b2369794377436770934d083cedffbb0
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-12-06 20:50:03 -0500

    m3

--- api-gateway/eureka/src/main/java/io/pivotal/microservices/eureka/Application.java
+++ api-gateway/eureka/src/main/java/io/pivotal/microservices/eureka/Application.java
@@ -1,14 +1,10 @@
 package io.pivotal.microservices.eureka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaServer
 public class Application {
 
--- api-gateway/gateway/src/main/java/io/pivotal/microservices/apigateway/Application.java
+++ api-gateway/gateway/src/main/java/io/pivotal/microservices/apigateway/Application.java
@@ -6,12 +6,10 @@ import io.pivotal.microservices.apigateway.services.recommendations.Recommendati
 import io.pivotal.microservices.apigateway.services.reviews.ReviewsIntegrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,31 +17,31 @@ import org.springframework.web.context.request.async.DeferredResult;
 import rx.Observable;
 import rx.Observer;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableHystrix
-@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+}
 
-
-    @Autowired
-    CatalogIntegrationService catalogIntegrationService;
-
-    @Autowired
-    ReviewsIntegrationService reviewsIntegrationService;
+@RestController
+class MovieRestController {
 
     @Autowired
-    RecommendationsIntegrationService recommendationsIntegrationService;
+    private CatalogIntegrationService catalogIntegrationService;
+
+    @Autowired
+    private ReviewsIntegrationService reviewsIntegrationService;
+
+    @Autowired
+    private RecommendationsIntegrationService recommendationsIntegrationService;
 
     @RequestMapping("/movie/{mlId}")
-    public DeferredResult<MovieDetails> movieDetails(@PathVariable String mlId) {
+    DeferredResult<MovieDetails> movieDetails(@PathVariable String mlId) {
         Observable<MovieDetails> details = Observable.zip(catalogIntegrationService.getMovie(mlId),
                 reviewsIntegrationService.reviewsFor(mlId),
                 recommendationsIntegrationService.getRecommendations(mlId), (movie, reviews, recommendations) -> {
@@ -58,7 +56,7 @@ public class Application {
         return toDeferredResult(details);
     }
 
-    public DeferredResult<MovieDetails> toDeferredResult(Observable<MovieDetails> details) {
+    private DeferredResult<MovieDetails> toDeferredResult(Observable<MovieDetails> details) {
         DeferredResult<MovieDetails> result = new DeferredResult<>();
         details.subscribe(new Observer<MovieDetails>() {
             @Override
@@ -76,4 +74,5 @@ public class Application {
         });
         return result;
     }
+
 }
