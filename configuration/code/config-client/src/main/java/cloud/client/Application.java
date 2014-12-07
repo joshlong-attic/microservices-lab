60a8a4955a913e60fb72cbfa8547f4807b23314d
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-12-06 19:53:43 -0500

    the config bits all run on CF

--- configuration/code/config-client/src/main/java/cloud/client/Application.java
+++ configuration/code/config-client/src/main/java/cloud/client/Application.java
@@ -1,12 +1,9 @@
 package cloud.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,45 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-
-    @Bean
-    @RefreshScope
-    Holder holder() {
-        return new Holder();
-    }
-
     public static void main(String[] args) {
-        Environment environment = SpringApplication.run(Application.class, args).getBean(Environment.class);
-        String projectName = environment.getProperty("configuration.projectName");
-        System.out.println(projectName);
+        SpringApplication.run(Application.class, args);
     }
 }
 
+@RefreshScope
 @RestController
-class HolderRestController {
-
-    @Autowired
-    private Holder holder;
-
-    @RequestMapping("/holder-value")
-    String string() {
-        return this.holder.getX();
-    }
-}
-
-class Holder {
+class ProjectRestController {
 
     @Value("${configuration.projectName}")
-    private String x;
+    private String projectName;
 
-    public String getX() {
-        return x;
-    }
-
-    @Override
-    public String toString() {
-        return "Holder{" +
-                "x='" + x + '\'' +
-                '}';
+    @RequestMapping("/project-name")
+    String projectName() {
+        return this.projectName;
     }
 }
