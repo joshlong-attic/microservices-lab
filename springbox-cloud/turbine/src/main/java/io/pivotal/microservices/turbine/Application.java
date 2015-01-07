deb0d63fe00f7ddfad238c1d8cafe93a5e936304
Matt Stine <mstine@gopivotal.com>
2015-01-07 14:36:03 -0600

    fix code that got deleted somehow

--- springbox-cloud/turbine/src/main/java/io/pivotal/microservices/turbine/Application.java
+++ springbox-cloud/turbine/src/main/java/io/pivotal/microservices/turbine/Application.java
@@ -1,7 +1,16 @@
 package io.pivotal.microservices.turbine;
 
-/**
- * Created by pivotal on 1/3/15.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
+
+@SpringBootApplication
+@EnableTurbineAmqp
+@EnableDiscoveryClient
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
