3c17bb60d64bffcc3ad64d90b548a62c26a35992
Matt Stine <mstine@gopivotal.com>
2015-01-13 22:57:43 -0800

    lattice config for gateway

---
+++ springbox-cloud/gateway/src/main/java/io/pivotal/microservices/apigateway/config/LatticeConfig.java
@@ -0,0 +1,29 @@
+package io.pivotal.microservices.apigateway.config;
+
+import io.pivotal.microservices.lattice.client.LatticeClient;
+import io.pivotal.microservices.lattice.model.LongRunningProcess;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("lattice")
+public class LatticeConfig {
+
+    @Value("${lattice.receptor.host}")
+    String receptorHost;
+
+    @Bean
+    public LatticeClient latticeClient() {
+        return new LatticeClient(receptorHost);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        LongRunningProcess lrp = latticeClient().findLongRunningProcess("rabbitmq");
+        return new CachingConnectionFactory(lrp.getAddress(), lrp.getPorts()[0].getHostPort());
+    }
+}
